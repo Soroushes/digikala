9c7243422c4b5f411f92d0deee9c12a5ed8a4b89
soroushes1 <esmailian.soroush246@gmail.com>
2022-09-05 00:24:19 +0430

    header creating

---
+++ src/header/ads.js
@@ -0,0 +1,6 @@
+const Ads = ({children})=>{
+    return(
+        <img className={"w-100"} src="http://localhost:3000/ads.jpg" alt={children}/>
+    )
+}
+export default Ads;
--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-
+import Header from "./header/header";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./styles/style.css" ;
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-);
-
+root.render(<Header/>);
