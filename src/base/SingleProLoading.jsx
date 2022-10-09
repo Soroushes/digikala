614214a96015c3b7a598c8279348aa7148623923
soroushes1 <esmailian.soroush246@gmail.com>
2022-10-09 22:55:59 +0330

    fixing bug

--- src/base/SingleProLoading.jsx
+++ src/base/SingleProLoading.jsx
@@ -5,8 +5,8 @@ const SingleProLoading = ()=>{
     return(
         <div style={{backgroundColor : "rgba(0,0,0,.1)"}} className="h-100 position-fixed start-0 top-0 w-100 d-flex justify-content-center align-items-center">
            <div className={'bg-white p-5 d-flex flex-column justify-content-around rounded-3 shadow'} style={{width : 350 , height : 400 }}>
-               <p className={'text-center'}><img src="https://www.digikala.com/statics/img/svg/logo.svg" alt="digikala-logo"/></p>
-               <p className={'text-center'}><Spin indicator={<LoadingOutlined style={{color : "red" , fontSize : 50}} spin/>}/></p>
+               <div className={'text-center'}><img src="https://www.digikala.com/statics/img/svg/logo.svg" alt="digikala-logo"/></div>
+               <div className={'text-center'}><Spin indicator={<LoadingOutlined style={{color : "red" , fontSize : 50}} spin/>}/></div>
                <p className={'text-center'}>لطفا صبر کنید...</p>
            </div>
         </div>
--- src/base/singleProImage.jsx
+++ src/base/singleProImage.jsx
@@ -1,23 +1,55 @@
 import {useSelector} from "react-redux";
+import {Swiper, SwiperSlide} from "swiper/react";
+import {FreeMode, Navigation, Thumbs} from "swiper";
+import {useState} from "react";
 
 const SingleProImage = ()=>{
     const {images} = useSelector(state => state.singleProduct) ;
+    const [thumbsSwiper, setThumbsSwiper] = useState(null);
     return(
         <>
-            <img className={'w-100'} src={images.main.url[0]} alt=""/>
-            <div className={'d-flex gap-2 flex-nowrap mt-2'}>
+            <Swiper
+                spaceBetween={0}
+                slidesPerView={1}
+                thumbs={{ swiper: thumbsSwiper }}
+                modules={[FreeMode,Thumbs]}
+                className="mySwiper2"
+            >
                 {
                     images.list.map((pics , index)=>{
                         return(
-                            index>5 ? "" :
-                                <div key={index} style={{width : "16.6666%"}} className={'border rounded-3 p-1'}>
+                            <SwiperSlide key={index}>
+                                <div className={'rounded-3 p-1'}>
                                     <img className={'w-100'} src={pics.url[0]} alt=""/>
                                 </div>
+                            </SwiperSlide>
                         )
                     })
                 }
-            </div>
+            </Swiper>
+            <Swiper
+                onSwiper={setThumbsSwiper}
+                spaceBetween={10}
+                slidesPerView={6}
+                freeMode={true}
+                watchSlidesProgress={true}
+                modules={[FreeMode, Navigation, Thumbs]}
+                className="mySwiper"
+            >
+                {
+                    images.list.map((pics , index)=>{
+                        return(
+                            <SwiperSlide key={index}>
+                                <div className={'border rounded-3 p-1'}>
+                                    <img className={'w-100'} src={pics.url[0]} alt=""/>
+                                </div>
+                            </SwiperSlide>
+                        )
+                    })
+                }
+            </Swiper>
         </>
+
 
     )
 }
