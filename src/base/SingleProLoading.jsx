02880f3757ef3dcfccd24f5928b68d6394138de8
soroushes1 <esmailian.soroush246@gmail.com>
2022-10-09 22:07:41 +0330

    fixing bug

--- src/base/SingleProLoading.jsx
+++ src/base/SingleProLoading.jsx
@@ -1,10 +1,12 @@
+import {LoadingOutlined} from "@ant-design/icons";
+import {Spin} from "antd";
 
 const SingleProLoading = ()=>{
     return(
         <div style={{backgroundColor : "rgba(0,0,0,.1)"}} className="h-100 position-fixed start-0 top-0 w-100 d-flex justify-content-center align-items-center">
            <div className={'bg-white p-5 d-flex flex-column justify-content-around rounded-3 shadow'} style={{width : 350 , height : 400 }}>
                <p className={'text-center'}><img src="https://www.digikala.com/statics/img/svg/logo.svg" alt="digikala-logo"/></p>
-               <p className={'text-center'}><img src="../svg/loading.svg" alt="loading"/></p>
+               <p className={'text-center'}><Spin indicator={<LoadingOutlined style={{color : "red" , fontSize : 50}} spin/>}/></p>
                <p className={'text-center'}>لطفا صبر کنید...</p>
            </div>
         </div>
